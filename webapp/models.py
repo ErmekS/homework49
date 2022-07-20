f6977d073d61dd65231b3d59a658f3ab695fa84f
ErmekSolpuev <ermeksee27@gmail.com>
2022-07-20 17:40:16 +0600

    Переделал модели задачи, статуса и типа.

--- webapp/models.py
+++ webapp/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
-
 # Create your models here.
+
+STATUS_CHOICES = [('new', 'Новая'), ('in_progress', 'В процессе'), ('done', 'Сделано')]
+TYPE_CHOICES = [('task', 'Задача'), ('bug', 'Ошибка'), ('enhancement', 'улучшение')]
+
+
 class BaseModel(models.Model):
     created_time = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
     updated_time = models.DateTimeField(auto_now=True, verbose_name="Дата изменения")
@@ -10,19 +14,37 @@ class BaseModel(models.Model):
         abstract = True
 
 
-STATUS_CHOICES = [('new', 'Новая'), ('in_progress', 'В процессе'), ('done', 'Сделано')]
-TYPE_CHOICES = [('task', 'Задача'), ('bug', 'Ошибка'), ('enhancement', 'улучшение')]
+class Status(models.Model):
+    status = models.CharField(max_length=35, choices=STATUS_CHOICES)
+
+    def __str__(self):
+        return f'{self.status}'
+
+    class Meta:
+        db_table = "status"
+        verbose_name = 'Статус'
+        verbose_name_plural = 'Статусы'
+
+
+class Type(models.Model):
+    type = models.CharField(max_length=35, choices=TYPE_CHOICES)
+
+    def __str__(self):
+        return f'{self.type}'
+
+    class Meta:
+        db_table = "types"
+        verbose_name = 'Тип'
+        verbose_name_plural = 'Типы'
 
 
 class Sketchpad(BaseModel):
     summary = models.CharField(max_length=50, null=False, blank=False, default="No summary",
                                verbose_name="Краткое описание")
     description = models.TextField(max_length=2000, null=True, blank=True, verbose_name="Полное описание")
-    status = models.CharField(max_length=20, on_delete=models.PROTECT, choices=STATUS_CHOICES,
-                              default=STATUS_CHOICES[0][0],
-                              verbose_name="Статус")
-    type = models.CharField(max_length=20, on_delete=models.PROTECT, choices=TYPE_CHOICES, default=TYPE_CHOICES[0][0],
-                            verbose_name="Тип")
+    status = models.ForeignKey("webapp.Status", on_delete=models.PROTECT, related_name='statuses',
+                               verbose_name='Статус')
+    type = models.ForeignKey("webapp.Type", on_delete=models.PROTECT, related_name="issues", verbose_name='Тип')
 
     def __str__(self):
         return f"{self.id}. {self.summary}: {self.status} {self.description} {self.created_time}"
