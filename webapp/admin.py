a7ab9db264c4cffd924a83943f0b2d573e206631
ErmekSolpuev <ermeksee27@gmail.com>
2022-07-20 19:10:54 +0600

    Создал админа и добавил через админ-панель типы и статусы задач, и 5-6 задач с разными статусами и типами. Создал фикстуру с данными

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from webapp.models import Sketchpad, Status, Type
+
+
+class SketchpadAdmin(admin.ModelAdmin):
+    list_display = ['id', 'summary', 'description', 'status', 'type', 'created_time', 'updated_time']
+    list_display_links = ['description']
+    list_filter = ['status', 'type']
+    search_fields = ['status']
+    fields = ['summary', 'description', 'status', 'type', 'created_time', 'updated_time']
+    readonly_fields = ['created_time', 'updated_time']
+
+
+admin.site.register(Sketchpad, SketchpadAdmin)
+admin.site.register(Type)
+admin.site.register(Status)
