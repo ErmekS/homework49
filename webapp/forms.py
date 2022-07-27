755678edb35f0123e18ada6ddc5240b195452f87
ErmekSolpuev <ermeksee27@gmail.com>
2022-07-27 22:59:54 +0600

    Создал отдельную ветку homework53. Применил ListView в проекте "Трекер" для представлений, выводящих списки объектов. Пагинировал список по 5 задач на страницу и добавьте поиск по задачам: по вхождению текста в краткое или полное описание.

--- webapp/forms.py
+++ webapp/forms.py
@@ -1,18 +1,22 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from django.forms import widgets
-from webapp.models import Sketchpad, Status, Type
-
-from webapp.validate import bad_words, bad_chars, check_count
+from webapp.models import Sketchpad
 
 
 class SketchpadForm(forms.ModelForm):
-    summary = forms.CharField(max_length=50, validators=(bad_chars,))
-    description = forms.CharField(max_length=2000, validators=(bad_words,),
-                                  widget=widgets.Textarea(attrs={"cols": 40, "rows": 3}))
-    type = forms.ModelMultipleChoiceField(queryset=Type.objects.all(), validators=(check_count,),
-                                          widget=forms.CheckboxSelectMultiple)
-    status = forms.ModelChoiceField(queryset=Status.objects.all(), empty_label='')
-
     class Meta:
         model = Sketchpad
-        fields = '__all__'
+        fields = ["summary", "description", "type", "status"]
+        widgets = {
+            "type": widgets.CheckboxSelectMultiple
+        }
+
+        def clean(self):
+            if self.cleaned_data.get("summary") == self.cleaned_data.get("description"):
+                raise ValidationError("Краткий заголовок и описание не могут совпадать")
+            return super().clean()
+
+
+class SearchForm(forms.Form):
+    search = forms.CharField(max_length=50, required=False, label='Поиск')
