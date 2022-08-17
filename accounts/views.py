f5c30da7ca35111fd521cd7220bef16d605c3bcc
ErmekSolpuev <ermeksee27@gmail.com>
2022-08-17 14:53:35 +0600

    создал новую ветку для дз 61,
    Добавил в проект страницы редактирования личных данных и смены пароля, доступные только владельцу учётной записи

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,15 @@
-from django.contrib.auth import authenticate, login, logout, get_user_model
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout, get_user_model, update_session_auth_hash
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.contrib.auth.views import PasswordChangeView
 from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, UpdateView
 
-from accounts.forms import MyUserCreationForm
+from accounts.forms import MyUserCreationForm, UserChangeForm, ProfileChangeForm, PasswordChangeForm
 from accounts.models import Profile
+
+User = get_user_model()
 
 
 class RegisterView(CreateView):
@@ -16,7 +17,7 @@ class RegisterView(CreateView):
     template_name = 'registration.html'
     form_class = MyUserCreationForm
 
-    def form_valid(self,form):
+    def form_valid(self, form):
         user = form.save()
         Profile.objects.create(user=user)
         login(self.request, user)
@@ -49,11 +50,13 @@ def logout_view(request):
     logout(request)
     return redirect('webapp:index')
 
+
 class ProfileView(LoginRequiredMixin, DetailView):
-    model = get_user_model()
+    model = User
     template_name = "profile.html"
     paginate_by = 6
     paginate_orphans = 0
+    context_object_name = "user_obj"
 
     def get_context_data(self, **kwargs):
         paginator = Paginator(self.get_object().projects.all(),
@@ -66,3 +69,57 @@ class ProfileView(LoginRequiredMixin, DetailView):
         context['projects'] = page_object.object_list
         context['is_paginated'] = page_object.has_other_pages()
         return context
+
+
+class ChangeProfileView(PermissionRequiredMixin, UpdateView):
+    model = User
+    form_class = UserChangeForm
+    template_name = "change_user.html"
+    profile_form_class = ProfileChangeForm
+    context_object_name = "user_obj"
+
+    def has_permission(self):
+        return self.request.user.is_superuser or self.request.user == self.get_object()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if "profile_form" not in context:
+            context["profile_form"] = self.profile_form_class(instance=self.get_object().profile)
+        return context
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.form_class(instance=self.object, data=request.POST)
+        profile_form = self.profile_form_class(instance=self.object.profile,
+                                               data=request.POST,
+                                               files=request.FILES)
+        if form.is_valid():
+            return self.form_valid(form, profile_form)
+        else:
+            return self.form_invalid(form, profile_form)
+
+    def form_valid(self, form, profile_form):
+        # self.get_form()
+        form.save()
+        profile_form.save()
+        return redirect("accounts:profile", self.object.pk)
+
+    def form_invalid(self, form, profile_form):
+        return self.render_to_response(self.get_context_data(form=form, profile_form=profile_form))
+
+
+class ChangePasswordView(PasswordChangeView):
+    # model = User
+    # form_class = PasswordChangeForm
+    template_name = "change_password.html"
+
+    # def get_object(self, queryset=None):
+    #     return self.request.user
+
+    def get_success_url(self):
+        return reverse("accounts:profile", kwargs={"pk": self.request.user.pk})
+
+    # def form_valid(self, form):
+    #     result = super().form_valid(form)
+    #     update_session_auth_hash(self.request, self.object)
+    #     return result
